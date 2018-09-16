76496f8a4088a335fe0e008fcec8a948ee65ce16
manson112 <rlarhksdn777@naver.com>
2018-09-17 00:23:59 +0900

    dfdf

--- drivers/lightnvm/pblk-recovery.c
+++ drivers/lightnvm/pblk-recovery.c
@@ -919,7 +919,7 @@ struct pblk_line *pblk_recov_l2p(struct pblk *pblk) {
       spin_unlock(&l_mg->gc_lock);
       continue;
     }
-    ppa.ppa = le64_to_cpu(line->snapshot->buf[lba++]);
+    ppa.ppa = line->snapshot->buf[lba++];
     pblk_update_map(pblk, (sector_t)lba, ppa);
   }
 
