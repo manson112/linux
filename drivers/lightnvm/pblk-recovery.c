a77eb7bfb839ad47754a3c989e81b47c695597aa
manson112 <rlarhksdn777@naver.com>
2018-09-26 15:39:44 +0900

    afd

--- drivers/lightnvm/pblk-recovery.c
+++ drivers/lightnvm/pblk-recovery.c
@@ -846,6 +846,7 @@ struct pblk_line *pblk_recov_l2p(struct pblk *pblk) {
     if (line->type == PBLK_LINETYPE_LOG) {
       snapshot_line_index = i;
       snapshot_line = line;
+      snapshot_line->type = PBLK_LINETYPE_DATA;
       bitmap_clear(line->erase_bitmap, 0, lm->blk_bitmap_len);
       printk("snapshot_line: [%d]%p\n", snapshot_line_index, snapshot_line);
     }
