9baae8b3c4375b31564a1c2dd110a17da3e1598e
manson112 <rlarhksdn777@naver.com>
2018-09-27 20:58:55 +0900

    add line

--- drivers/lightnvm/pblk-write.c
+++ drivers/lightnvm/pblk-write.c
@@ -257,6 +257,7 @@ static void pblk_end_io_write(struct nvm_rq *rqd) {
 static void pblk_end_io_write_snapshot(struct nvm_rq *rqd) {
   struct pblk *pblk = rqd->private;
   struct pblk_g_ctx *s_ctx = nvm_rq_to_pdu(rqd);
+  struct pblk_line *line = s_ctx->private;
   pblk_up_page(pblk, rqd->ppa_list, rqd->nr_ppas);
 
   if (rqd->error) {
